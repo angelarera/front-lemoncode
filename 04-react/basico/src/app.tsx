e11fa530027f50b19553e55178d8be56dfb2a4a3
Ángela Real <angelarealr@gmail.com>
2025-10-21 17:47:48 +0200

    Comienzo laboratorio React básico

--- 04-react/basico/src/app.tsx
+++ 04-react/basico/src/app.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { AppRouter } from "./router";
-import CssBaseline from "@mui/material/CssBaseline";
 
 export const App: React.FC = () => {
-  return (
-    <>
-      <CssBaseline />
-      <AppRouter />
-    </>
-  );
+  return <AppRouter />;
 };
